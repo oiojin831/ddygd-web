72f2524ad17c6d6acc14da3445ddda81f4c83bd0
Eung Jin Lee <oiojin831@gmail.com>
2020-01-29 16:47:36 +0900

    Basic working reactfire

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { SuspenseWithPerf, useFirestoreDocData, useFirestore } from "reactfire";
+
+function Burrito() {
+  // lazy load the Firestore SDK
+  const firestore = useFirestore();
+
+  // create a document reference
+  const burritoRef = firestore()
+    .collection("tryreactfire")
+    .doc("burrito");
+
+  // subscribe to the doc. just one line!
+  const burrito = useFirestoreDocData(burritoRef);
+
+  // get the value from the doc
+  const isYummy = burrito.yummy;
+  const eungjin = burrito.eungjin;
+
+  return <div>
+    <p>The burrito is {isYummy ? "good" : "bad"}</p>
+    <p>The eungjin is {eungjin}</p>
+  </div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SuspenseWithPerf
+        fallback={"loading burrito status..."}
+        traceId={"load-burrito-status"}
+      >
+        <Burrito />
+      </SuspenseWithPerf>
     </div>
   );
 }
